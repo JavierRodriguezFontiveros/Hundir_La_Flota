82cc4df92fe1a3947b96d79082a1548f8d259103
JavierRodriguezFontiveros <javierrodriguezfontiveros@gmail.com>
2024-09-28 22:14:26 +0200

    Nuevas_actualizaciones_Main_py

--- Main1.0.py
+++ Main1.0.py
@@ -1,8 +1,7 @@
-
-
 import time
 import numpy as np
 import utils
+
 def jugar():
     print(f"Welcome to Battleship")
     time.sleep(1)
@@ -11,7 +10,9 @@ def jugar():
 
     time.sleep(2)
 
-    modo = ("Hay dos modos de juego: Corto( Cada jugador tendra 6 barcos de eslora 1 y quién elimine antes dos barcos habrá ganado. Largo: 6 barcos cada uno y quien elimine los del contrincante antes habrá ganado.  Introduzca: (Corto/Largo)) ")
+    modo = input("Hay dos modos de juego: Corto( Cada jugador tendra 6 barcos de eslora 1 y quién elimine\
+                 \ antes dos barcos habrá ganado. Largo: 6 barcos cada uno y quien elimine los del\
+                  contrincante antes habrá ganado.  Introduzca: (Corto/Largo)) ").lower()
 
     print(f"Creating your new the game track...")
 
@@ -19,42 +20,42 @@ def jugar():
     tablero_usuario = utils.crear_tablero(10)
     tablero_maquina = utils.crear_tablero(10)
 
-    modo = ("Hay dos modos de juego: Corto( Cada jugador tendra 6 barcos de eslora 1 y quién elimine antes dos barcos habrá ganado. Largo: 6 barcos cada uno y quien elimine los del contrincante antes habrá ganado.  Introduzca: (Corto/Largo)) ")
+    
     if modo == "corto":
-        flota_usuario = utils.crear_barcos_modo_corto(tablero_usuario)
-        flota_maquina = utils.crear_flota(tablero_maquina)
+        flota_usuario = [utils.crear_barcos(1,tablero_usuario) for _ in range(6)]
+        flota_maquina = [utils.crear_barcos(1,tablero_maquina) for _ in range(6)]
     else:
         flota_usuario = utils.crear_flota(tablero_usuario)
         flota_maquina = utils.crear_flota(tablero_maquina)
-        #print(flota_usuario) if you want to see yours boats before placing in the game track
-
+        
     time.sleep(0.5)
     print(f"Your fleet will be made up of 6 ships and they will be placed randomly on the game track.")
-
-
-    tablero_final = utils.colocar_flota(flota_usuario,tablero_usuario)
-    tablero_maquina_final = utils.colocar_flota(flota_maquina,tablero_maquina)
-    print(f"\n               Here you are")
-    print(tablero_final)
+    print(tablero_usuario)
 
     while True: 
-        respuesta = input("Te gusta tu tablero o generamos otro?(True/False)")    
-        if respuesta == "True":
+        respuesta = input("Te gusta tu tablero o generamos otro?(si/no)").lower()    
+        if respuesta == "si":
             print(f"Then you will play with this, GOOD LUCK!")
             break
         else:
-            print("Generando nuevo tablero")
-            time.sleep(1)
-            tablero_final = utils.generar_nuevo_tablero()
-            print(tablero_final)
+            tablero_usuario = utils.crear_tablero(10)
+            flota_usuario = utils.crear_flota(tablero_usuario)
+            utils.colocar_flota(flota_usuario,tablero_usuario)
+            print("Nuevo tablero")
+            print(tablero_usuario)
+
+
+            
         
-    utils.sistema_de_turnos(tablero_final, tablero_maquina)
+    utils.sistema_de_turnos(tablero_usuario, tablero_maquina, flota_usuario, flota_maquina, modo)
 
 def hundir_la_flota():
     while True:
         jugar()
-        jugar_nuevamente = input("¿Quieres jugar de nuevo? (True/False): ").lower()
-        if jugar_nuevamente != "True":
+        jugar_nuevamente = input("¿Quieres jugar de nuevo? (si/no): ").lower()
+        if jugar_nuevamente != "si":
             print(f"Saliendo del juego, que tenga buen día no dude en volver a jugar")
             print(f"Con un pull de git compruebe las nuevas actualizaciones")
             break
+
+hundir_la_flota()
--- utils.py
+++ utils.py
@@ -43,7 +43,7 @@ def crear_barco(eslora, tablero):
             if not comprobar_colision(barco, tablero):
                 return barco
         
-def crear_barco_modo_corto(tablero)
+def crear_barco_modo_corto(tablero):
     barcos=[]
     for _ in range(6):
         barco = [(random.randint(0, 9), random.randint(0, 9))]
@@ -69,8 +69,8 @@ def crear_flota(tablero):
         crear_barco(4, tablero)]
     return flota_barcos
 
-def colocar_flota(flota, tablero): 
-    for barco in flota:
+def colocar_flota(barcos, tablero): 
+    for barco in barcos:
         for i, j in barco:
             tablero[i][j] = 'O'  
     return tablero
@@ -81,75 +81,75 @@ def generar_nuevo_tablero():
     tablero_final = colocar_flota(flota_usuario, tablero_usuario)
     return tablero_final
 
-#Hasta aquí funciones para preparar el juego
-#Apartir de aqui empieza el sistema de turnos
+def sistema_de_turnos(tablero_final, tablero_maquina, flota_usuario, flota_maquina, modo):
 
-def sistema_de_turnos(tablero_final, tablero_maquina):
     print("Que comience el juego")
-    puntos_usuario = 0
-    puntos_maquina = 0
+    barcos_hundidos_usuario = 0
+    barcos_hundidos_maquina = 0
     if modo == "corto":
         limite_puntos = 2
     else:
         limite_puntos = len(tablero_final[tablero_final == "O"])
     
-    while puntos_usuario < limite_puntos and puntos_maquina < limite_puntos:
+    while barcos_hundidos_usuario < limite_puntos and barcos_hundidos_maquina < limite_puntos:
         print("\n--- Tu turno ---")    
-        puntos_usuario += turno_usuario(tablero_maquina)
-        
-        if puntos_usuario > limite_puntos:   
+        barcos_hundidos_maquina += turno_usuario(tablero_maquina)
+        mostrar_tablero(tablero_maquina)
+
+        if barcos_hundidos_maquina >= limite_puntos:   
             print("Has ganado makena.")
             break
         time.sleep(1)
 
         print("\n--- Turno de la Máquina ---")   
-        puntos_maquina += turno_maquina(tablero_final)
+        barcos_hundidos_usuario += turno_maquina(tablero_final, flota_usuario)
         print(tablero_final)
 
-        if puntos_maquina >= limite_puntos:      
+        if barcos_hundidos_usuario >= limite_puntos:      
             print("La máquina te ha ganado crack")
             break
 
 def verificar_victoria(tablero_oponente,modo):
-    if modo == "Corto":
+    if modo == "corto":
         barcos_restantes = len(np.where(tablero_oponente == "O")[0])
         barcos_hundidos = 6 - barcos_restantes
         return barcos_hundidos >= 2 #Gana si almenos se hunden dos barcos
     else:
         return not np.any(tablero_oponente == "O") #Gana si no queda ningun barco
 
-def turno_usuario(tablero):
+def turno_usuario(tablero,flota_maquina):
     while True:  
         try:                                                                 #Mientras no se pierda el turno
-            fila = int(input("Introduce la fila (1-9) a la que quieres disparar: "))
-            columna = int(input("Introduce la columna (1-9) a la que quieres disparar: "))
-            if not (0 <= fila <= 9) or not columna (0 <= columna <= 9):
+            fila = int(input("Introduce la fila (1-10) a la que quieres disparar: "))
+            columna = int(input("Introduce la columna (1-10) a la que quieres disparar: "))
+            if not (0 <= fila <= 9) or not (0 <= columna <= 9):
                 print("Cordenadas fuera de rango, intentelo de nuevo")
                 continue
         except ValueError:
             print("Intruce numero entre el 1 y 9.")
             continue
 
-        resultado = disparar((fila-1, columna-1), tablero)             #llamada ala función disparar
-        print(f"Disparaste a {(fila, columna)}:\n {resultado}")     #Ubicación del tiro por pantalla
+        resultado = disparar((fila, columna), tablero)             #llamada ala función disparar
+        print(f"Disparaste a {(fila + 1, columna + 1)}:\n {resultado}")     #Ubicación del tiro por pantalla
         
         if resultado == "Agua":                                     #Posibilidades
             print("Fallaste. Fin de tu turno.")
             return 0
         elif resultado == "Tocado":
             print("Has acertado sigue disparando.")
-            if barco_hundido(tablero):
-                print("Barco Hundido")
-                return 1
-            
+            for barco in flota_maquina:
+                if barco_hundido(barco, tablero):
+                    print("Barco Hundido")
+                    return 1
+                
 
-def turno_maquina(tablero):
+def turno_maquina(tablero, flota_usuario):
     while True:                      #Mientras acierte seguirá disparando de forma random
         fila = random.randint(0, 9)
         columna = random.randint(0, 9)
         
         resultado = disparar((fila, columna), tablero)
-        print(f"La máquina disparó a {(fila, columna)}: \n {resultado}")
+        print(f"La máquina disparó a {(fila + 1, columna + 1)}: \n {resultado}")
         
         if resultado == "Agua":
             print("La máquina falló. Fin de su turno.")
@@ -157,30 +157,32 @@ def turno_maquina(tablero):
 
         elif resultado == "Tocado":
             print("La máquina acertó. Sigue disparando.")
-            return 1
+            for barco in flota_usuario:
+                if barco_hundido(barco,tablero):
+                    print("Barco Hundido")
+                    return 1
         
 
 def disparar(casilla, tablero):
     fila, columna = casilla
     if tablero[fila, columna] == "O":              #la O es barco, la X tocado y la A agua
-        print("Tocado")
         tablero[fila, columna] = "X"
         return "Tocado"
     elif tablero[fila,columna] == '_': 
-        print("Agua")
         tablero[fila,columna] = "A"
         return "Agua"
     else:
-        print("Casilla ya disparada.")
         return "Casilla repetida"
     
+def barco_hundido(barco, tablero):
+    for i,j in barco:
+        if tablero[i][j] != "X":
+            return False
+    return True
 
-def barco_hundido(tablero,fila,columna):
-    return not np.any(tablero == "O")
+def mostrar_tablero(tablero):
+    print("\n--- Tablero de la maquina ---")
+    for fila in tablero:
+        print(" ".join(fila))
 
-def crear_barcos_modo_corto(tablero):
-    barcos = []
-    for _ in range(6):
-        barco = crear_barco(1, tablero)  
-        barcos.append(barco)
-    return barcos
+
